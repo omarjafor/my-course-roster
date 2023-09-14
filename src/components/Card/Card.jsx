b69128d87a6ebac32c77e65f2ae89a7053d5a93e
omarjafor <omarjaforchy@gmail.com>
2023-09-14 22:02:26 +0600

    first static card design completed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -9,13 +9,13 @@ const Card = () => {
                     <p className="text-sm font-normal leading-6 mb-5 text-[#1C1B1B99]">
                             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
                     </p>
-                    <div className="flex justify-around my-2 font-bold text-lg">
-                        <p>Price: 15000$</p>
-                        <p>Credit: 2hr</p>
+                    <div className="flex justify-around my-2 font-medium text-base gap-3 text-[#1C1B1B99]">
+                        <p><img className="inline-block mr-2" src="../../../public/dollar.svg" alt="" width="24px" height="24px" />Price : 15000</p>
+                        <p><img className="inline-block mr-2" src="../../../public/credit.svg" alt="" width="24px" height="24px" />Credit : 10hr</p>
                     </div>
                 </div>
                 <button
-                    className="w-full font-bold rounded-lg p-2 mt-1 bg-blue-400 text-white border border-solid border-transparent cursor-pointer]"
+                    className="w-full font-semibold rounded-lg p-2 mt-1 bg-[#2F80ED] text-white cursor-pointer text-lg"
                 >Select</button>
             </div>
         </div>
