2df988905e60a921d7e09f1a6ce3907277d1de4e
omarjafor <omarjaforchy@gmail.com>
2023-09-14 23:38:30 +0600

    card design and maping all card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,24 +1,24 @@
-const Card = () => {
+const Card = ({course}) => {
+    const {image, name, price, credit, about} = course;
+    
     return (
-        <div>
             <div className="w-[312px] h-[402px] shadow-lg p-4 rounded-lg bg-[#FFF] text-center">
                 <figure className="bg-[#1111110D] w-[280px] h-[144px] text-center rounded-lg"><img
-                    src="https://i.ibb.co/qJDdBpC/Rectangle-2.png" alt="Tea" /></figure>
+                    src={image} alt="Course" /></figure>
                 <div className="text-left">
-                    <h2 className="text-lg font-semibold mt-4 mb-3"> Introduction to C Programming </h2>
+                    <h2 className="text-lg font-semibold mt-4 mb-3"> {name} </h2>
                     <p className="text-sm font-normal leading-6 mb-5 text-[#1C1B1B99]">
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                        {about}
                     </p>
                     <div className="flex justify-around my-2 font-medium text-base gap-3 text-[#1C1B1B99]">
-                        <p><img className="inline-block mr-2" src="../../../public/dollar.svg" alt="" width="24px" height="24px" />Price : 15000</p>
-                        <p><img className="inline-block mr-2" src="../../../public/credit.svg" alt="" width="24px" height="24px" />Credit : 10hr</p>
+                        <p><img className="inline-block mr-2" src="../../../public/dollar.svg" alt="" width="24px" height="24px" />Price : {price}</p>
+                        <p><img className="inline-block mr-2" src="../../../public/credit.svg" alt="" width="24px" height="24px" />Credit : {credit}hr</p>
                     </div>
                 </div>
                 <button
                     className="w-full font-semibold rounded-lg p-2 mt-1 bg-[#2F80ED] text-white cursor-pointer text-lg"
                 >Select</button>
             </div>
-        </div>
     );
 };
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 import Card from "../Card/Card";
 
 const Home = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setCourses(data));
+    }, [])
     return (
-        <div className="flex">
-            <Card></Card>
+        <div className="grid grid-cols-3 gap-6">
+            {
+                courses.map(course => (<Card
+                    key={course.id}
+                    course={course}
+                ></Card>))
+            }
         </div>
     );
 };
